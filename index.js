58ab1fa95365ac652c1fdd4b11c6f072df68004e
Ethan-Hatch <ehatch@utah.gov>
2022-12-29 15:49:15 -0700

    changes to index.js

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ try {
     var newJson = core.getInput('json');
     console.log('JSON data: ' + newJson);
 
-    // fetch (newUrl, {
-    //     body: JSON.stringify(newJson),
-    //     headers: {
-    //         'dataType' : 'json',
-    //         'content-type' : 'application/json'
-    //     },
-    //     method: 'POST',
-    //     redirect: 'follow'
-    // })
-    // .then(response => {
-    //     if (response.status === 200) {
-    //         console.log(response.text());
-    //     } else {
-    //         throw new Error('Api server had an issue');
-    //     }
-    // })
-    // .catch(error => {
-    //     console.error(error);
-    // })
+    fetch (newUrl, {
+        body: JSON.stringify(newJson),
+        headers: {
+            'dataType' : 'json',
+            'content-type' : 'application/json'
+        },
+        method: 'POST',
+        redirect: 'follow'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            console.log(response.text());
+        } else {
+            throw new Error(response.status);
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    })
 
-    async function curlPost() {
-        let response = await fetch(newUrl, {
-            method: 'POST',
-            headers: { 'content-type': 'application/json'},
-            body: JSON.stringify(newJson)
-        });
-    }
+    // async function curlPost() {
+    //     let response = await fetch(newUrl, {
+    //         method: 'POST',
+    //         headers: { 'content-type': 'application/json'},
+    //         body: JSON.stringify(newJson)
+    //     });
+    // }
 
 } catch (error) {
     core.setFailed(error.message);
